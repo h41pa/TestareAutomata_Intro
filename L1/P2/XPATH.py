b75345f2558bfb3941ff3aed57d0be1c64da994d
h41pa <madalin_chelu@yahoo.com>
2023-10-10 16:07:49 +0300

    push ~

--- L1/P2/XPATH.py
+++ L1/P2/XPATH.py
@@ -1,3 +1,8 @@
+"""
+
+
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -13,5 +18,15 @@ driver.find_element(By.XPATH, "(//input)[1]")
 
 # Cauta al doilea element de tip input
 driver.find_element(By.XPATH, "(//input)[2]")
+
+# Al doilea copil cu tag-ul <div> al unui element parinte oarecare - se observa ca parintele nu este specificat
+driver.find_element(By.XPATH, "//div[2]")
+
+# Al doilea copil cu tag-ul ORICARE * al unui element parinte oarecare, cu clasa 'form-control'
+driver.find_element(By.XPATH, "//*[2][@class='form-control']")
+
+# Primul element copil cu tag-ul <input> cu id='last-name' al unui parinte <div>
+driver.find_element(By.XPATH, "//div/input[1][@id='last-name']")
+
 
 sleep(5)
