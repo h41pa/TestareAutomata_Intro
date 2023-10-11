5547e2e5d64ed5b1ead75ee34202e75ada26a031
h41pa <madalin_chelu@yahoo.com>
2023-10-11 10:54:54 +0300

    push ~

--- L1/P2/XPATH.py
+++ L1/P2/XPATH.py
@@ -28,5 +28,27 @@ driver.find_element(By.XPATH, "//*[2][@class='form-control']")
 # Primul element copil cu tag-ul <input> cu id='last-name' al unui parinte <div>
 driver.find_element(By.XPATH, "//div/input[1][@id='last-name']")
 
+# Ultimul element copil cu tag-ul <option> al unui element parinte <select>
+driver.find_element(By.XPATH, "//select/option[last()]")
+
+# Penultimul element copil cu tag-ul <option> al unui element parinte <select>
+driver.find_element(By.XPATH, "//select/option[last()-1]")
+
+# Simbolul | (pipe) - se foloseste intre 2 XPATH-uri ---
+# Simbolul | (pipe) - sau logic
+# input cu id=id-inexistent sau input cu id=first-name
+driver.find_element(By.XPATH, "//input[@id='id-inexistent'] | //input[@id='first-name']")
+
+# Simbolul or - se foloseste intre 2 attribute ---
+# Simbolul or - sau logic
+# input cu id=first sau id=last
+driver.find_element(By.XPATH, "//input[contains(@id,'first') or contains(@id,'last')]")
+
+# Simbolul and - se foloseste intre 2 atribute
+# Simbolul and - si logic
+# input cu id=frist si id=name
+driver.find_element(By.XPATH, "//input[contains(@id,'first') and contains(@id,'name')]")
+
+
 
 sleep(5)
