ccc4838fb508e24a9b334b5357a4f53d04b5b038
h41pa <madalin_chelu@yahoo.com>
2023-11-06 11:31:57 +0200

    push ~

--- L5/P1/tests/test_update_order.py
+++ L5/P1/tests/test_update_order.py
@@ -14,13 +14,15 @@ class TestUpdateOrder(unittest.TestCase):
     def test_update_order_status_code(self):
         book_id = 1
         customer_name = "Dementor"
-        place_order_resp = self.books_api.submit_order(self.acces_token, book_id, customer_name)
-        status_code =  201
-        expected_status = place_order_resp.status_code
-        self.assertEqual(status_code, expected_status, " Error , unexpected statsu code")
-        order_id = place_order_resp.json()['orderId']
-        new_costumer_name = "Bujie"
-        update_order_resp = self.books_api.update_order(self.acces_token, order_id, new_costumer_name)
+
+        submit_order_reponse = self.books_api.submit_order(self.acces_token, book_id, customer_name)
+        expected_status_code = 201
+        actual_status_code = submit_order_reponse.status_code
+        self.assertEqual(expected_status_code, actual_status_code, "Error, Not created")
+        order_id = submit_order_reponse.json()['orderId']
+        new_customer_name = "Daniel"
+        update_order_response = self.books_api.update_order(self.acces_token, order_id, new_customer_name)
         expected_status_code = 204
-        actual_Status_code = update_order_resp.status_code
-        self.assertEqual(expected_status_code, actual_Status_code, "Error , unexpected status")
+        actual_status_code = update_order_response.status_code
+        self.assertEqual(expected_status_code, actual_status_code, "Unexpected Status Code")
+
